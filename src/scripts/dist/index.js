1f7d7d04fb8cb74d8e811ce09c7aa6e62477a941
Anish Kumar <akumar@techrayslabs.com>
2026-02-09 01:55:14 +0530

    added hamburger icon and responsivenss for smaller screens

--- src/scripts/dist/index.js
+++ src/scripts/dist/index.js
@@ -1,3 +1,10 @@
-console.log("Inventory Management");
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.getElementById("sidebar");
+let flag = true;
+function menuBtnfunct() {
+    console.log("click");
+    sidebar?.classList.toggle("hidden");
+}
+window.menuBtnfunct = menuBtnfunct;
 export {};
 //# sourceMappingURL=index.js.map
--- src/scripts/src/index.ts
+++ src/scripts/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Inventory Management");
+
+
+const menuBtn = document.getElementById("menu-btn") as HTMLButtonElement | null;
+const sidebar = document.getElementById("sidebar") as HTMLElement | null;
+let flag:boolean = true
+
+function menuBtnfunct(){
+  
+    
+  console.log("click");
+  sidebar?.classList.toggle("hidden")
+   
+}
+
+
+
+
+(window as any).menuBtnfunct = menuBtnfunct;
